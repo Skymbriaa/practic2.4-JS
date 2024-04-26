a8f3271d1bb95a119173f2c75c7d9e1da9f56455
vova vasylenko <pozilaaskymbria@gmail.com>
2024-04-26 10:30:24 +0300

    Add 'task'

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log([]);
+
+const arrNums1 = [4,5,6];
+
+console.log('length', arrNums1.unshift(7));
+console.log(arrNums1.shift());
+console.log(arrNums1);
+
+//
+const dayWeeks = ['sun' ,'mon','tue','wen','thu','fri','sat'];
+
+console.log(dayWeeks.pop());
+console.log(dayWeeks.pop());
+console.log(dayWeeks.pop());
+console.log(dayWeeks.unshift('sun' ,'mon','tue','wen','thu','fri','sat'));
+console.log(dayWeeks.unshift('sun' ,'mon','tue','wen','thu','fri','sat'));
+console.log(dayWeeks.push('fri'));
+console.log(dayWeeks.shift());
+
+console.log(dayWeeks);
