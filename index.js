82641c4baf14c0e0ecf760fbc7a714008851b240
vova vasylenko <pozilaaskymbria@gmail.com>
2024-04-26 12:13:50 +0300

    Add 'key'

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 
-const colors = ['red','white','pink','aqua'];
+// // const colors = ['red','white','pink','aqua'];
 
-colors.splice(2, 0, 'green');
-colors.splice(4, 1, 'blue');
-colors.splice(1, 0, 'mangeta, green');
-colors.splice()
+// // colors.splice(2, 0, 'green');
+// // colors.splice(4, 1, 'blue');
+// // colors.splice(1, 0, 'mangeta, green');
+// // colors.splice()
 
-console.log(colors)
+// // console.log(colors);
+
+// const arrNums1 = [4,5,6,7,8,9];
+// console.log(arrNums1);
+
+// const elem = 7;
+// const indexFirstElem = arrNums1.indexOf(elem);
+// const indexLastElem = arrNums1.lastIndexOf(elem);
+
+// console.log(indexFirstElem);
+// console.log(indexLastElem);
+
+
+const colors = [1,2,3,5,4,2,3,5,7,4,3,2];
+const key = 4;
+console.log(colors);
+console.log(colors.lastIndexOf(key));
+console.log(colors.indexOf(key));
+
+
